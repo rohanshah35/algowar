3e488f8ea28cc5c518f57db4ed00a4c5d11b82c7
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2024-12-29 19:08:18 -0500

    updated footer

--- frontend/src/app/components/footer.tsx
+++ frontend/src/app/components/footer.tsx
@@ -25,26 +25,50 @@ export function Footer() {
   return (
     <div className={classes.footer}>
       <div className={classes.inner}>
-        <Text size="sm" style={{ letterSpacing: '2px', fontFamily: inter.style.fontFamily }}> 
+        <Text size="xs" style={{ letterSpacing: '2px', fontFamily: inter.style.fontFamily }}> 
           algowar.xyz
         </Text>
 
         <Group className={classes.links} style={{ fontSize: '12px', fontFamily: inter.style.fontFamily }}>
-          Created by Luca Bianchini and Rohan Shah
+          Created by
+          <Anchor
+            size="xs"
+            href="https://github.com/lfbianchini"
+            target="_blank"
+            rel="noopener noreferrer"
+            underline="never"
+          >
+            Luca Bianchini
+          </Anchor>
+          and
+          <Anchor
+            size="xs"
+            href="https://github.com/rohanshah35"
+            target="_blank"
+            rel="noopener noreferrer"
+            underline="never"
+          >
+            Rohan Shah
+          </Anchor>
         </Group>
 
         <Group justify="center">
-          <ActionIcon 
-            size="md" 
-            variant="default" 
-            radius="xl" 
-            component="a" 
-            href="https://github.com/rohanshah35/algowar"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconBrandGithub size={18} stroke={2} />
-          </ActionIcon>
+        <ActionIcon
+          size="md"
+          variant="filled"
+          color="dark"
+          radius="xl"
+          component="a"
+          href="https://github.com/rohanshah35/algowar"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            backgroundColor: '#1A1B1E',
+            transition: 'none',
+          }}
+        >
+          <IconBrandGithub size={18} stroke={2} color="white" />
+        </ActionIcon>
         </Group>
       </div>
     </div>
