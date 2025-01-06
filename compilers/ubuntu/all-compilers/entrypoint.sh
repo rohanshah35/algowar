2624b4a77a17c712d8ad33b93ceb80f1a0b243e2
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2025-01-06 00:02:24 -0500

    separate docker images for each compiler

--- compilers/ubuntu/all-compilers/entrypoint.sh
+++ compilers/ubuntu/all-compilers/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
 if [ "$#" -lt 2 ]; then
   echo "Usage: $0 <language> <source_file>"
   exit 1
@@ -10,10 +9,8 @@ LANGUAGE=$1
 SOURCE_FILE=$2
 OUTPUT_FILE="output"
 
-# Move to the workspace directory
 cd /workspace
 
-# Compile or interpret the code based on the language
 case $LANGUAGE in
   "c")
     gcc "$SOURCE_FILE" -o "$OUTPUT_FILE"
@@ -41,7 +38,6 @@ case $LANGUAGE in
     ;;
 esac
 
-# Execute the compiled output if it's a C or C++ program
 if [ -f "$OUTPUT_FILE" ]; then
   ./"$OUTPUT_FILE"
 fi
