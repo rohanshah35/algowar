d0c6d5c513d9a4b09546195ddd547d7f17cb1867
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2025-01-06 00:30:34 -0500

    containers migrated to alpine

--- compilers/alpine/java-compiler/entrypoint.sh
+++ compilers/alpine/java-compiler/entrypoint.sh
@@ -12,20 +12,6 @@ OUTPUT_FILE="output"
 cd /workspace
 
 case $LANGUAGE in
-  "c")
-    gcc "$SOURCE_FILE" -o "$OUTPUT_FILE"
-    ;;
-  "cpp" | "c++")
-    g++ "$SOURCE_FILE" -o "$OUTPUT_FILE"
-    ;;
-  "python")
-    python2 "$SOURCE_FILE"
-    exit 0
-    ;;
-  "python3")
-    python3 "$SOURCE_FILE"
-    exit 0
-    ;;
   "java")
     javac "$SOURCE_FILE"
     OUTPUT_FILE=$(basename "$SOURCE_FILE" .java)
